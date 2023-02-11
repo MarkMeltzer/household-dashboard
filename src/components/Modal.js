d14bf2c0f40d5046987b143324bd681aafda86bd
MarkMeltzer <33347173+MarkMeltzer@users.noreply.github.com>
2023-02-11 13:19:29 +0100

    Allow passing of modal activator style

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -2,33 +2,34 @@ import React, { useState, useEffect } from "react";
 import { createPortal } from "react-dom";
 import "../css/components/Modal.css";
 
-function Modal({ children, activator }) {
+function Modal({ children, activator, activatorStyle }) {
   const [showModal, setShowModal] = useState(false);
 
   const clickOutside = (e) => {
     if (e.target.classList.contains("modal-container")) {
       setShowModal(false);
     }
-  }
+  };
 
   const clickActivator = (e) => {
     setShowModal(true);
-  }
+  };
 
   return (
     <>
-      <div className="activator" onClick={clickActivator}>{activator}</div>
+      <div className="activator" onClick={clickActivator} style={activatorStyle}>
+        {activator}
+      </div>
       {showModal &&
         createPortal(
           <div className="modal-container" onClick={clickOutside}>
             <div className="modal">
-              <button
-                onClick={() => setShowModal(false)}
-                className="close-button"
-              >
-                Close
-              </button>
-              {children}
+              <div>
+                <button onClick={() => setShowModal(false)} className="close-button">
+                  X
+                </button>
+              </div>
+              <div>{children}</div>
             </div>
           </div>,
           document.body
