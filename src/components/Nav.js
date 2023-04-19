f1e92bd3cf7e6e6672b498c715ce9d9dcf3351df
MarkMeltzer <33347173+MarkMeltzer@users.noreply.github.com>
2023-04-20 00:15:27 +0200

    Make navbar responsive
    HHD-17

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,25 +1,87 @@
-import { useContext } from 'react';
-import { Link } from 'react-router-dom';
-import globalContext from "../globalContext";
-import '../css/components/Nav.css';
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import "../css/components/Nav.css";
+import useWindowSize from "../hooks/useWindowSize";
 
 const Nav = ({ setLoginToken }) => {
-  const context = useContext(globalContext);
+  const windowSize = useWindowSize();
+  const [overflowOpen, setOverflowOpen] = useState(false);
+  const [overflowCuttoff, setOverflowCuttoff] = useState(2);
 
-  function logout() {
+  const links = [
+    { location: "/", label: "Home" },
+    { location: "/newweek", label: "New Week" },
+    { location: "/playground", label: "Playground" },
+    { location: "/shoppingItems", label: "Shopping Items" },
+  ];
+
+  useEffect(() => {
+    if (windowSize.width < 600) {
+      setOverflowCuttoff(3);
+    } else if (windowSize.width < 1200) {
+      setOverflowCuttoff(2);
+    } else if (windowSize.width < 1500) {
+      setOverflowCuttoff(3);
+    } else if (windowSize.width < 1900) {
+      setOverflowCuttoff(4);
+    } else {
+      setOverflowCuttoff(100);
+    }
+  }, [windowSize]);
+
+  function logout(event) {
+    event.preventDefault();
+
     localStorage.removeItem("loginToken");
     setLoginToken(null);
   }
 
+  function toggleOverflow(event) {
+    event.preventDefault();
+
+    setOverflowOpen((prev) => !prev);
+  }
+
   return (
     <nav>
-      <Link to="/">Home</Link>
-      <Link to="/newweek">New Week</Link>
-      <Link to="/playground">Playground</Link>
-      <Link to="/shoppingItems">Shopping items</Link>
-      <button onClick={logout}>Logout!</button>
+      <div className="menu">
+        {links.slice(0, overflowCuttoff).map((link) => {
+          return (
+            <Link
+              to={link.location}
+              key={link.location + link.label}
+              onClick={() => setOverflowOpen(false)}
+            >
+              {link.label}
+            </Link>
+          );
+        })}
+        <a href="" onClick={logout}>
+          Logout!
+        </a>
+        {overflowCuttoff < links.length && (
+          <a href="" onClick={toggleOverflow}>
+            {overflowOpen ? "≢" : "≡"}
+          </a>
+        )}
+      </div>
+      {overflowOpen && (
+        <div className="overflowMenu">
+          {links.slice(overflowCuttoff).map((link) => {
+            return (
+              <Link
+                to={link.location}
+                key={link.location + link.label}
+                onClick={() => setOverflowOpen(false)}
+              >
+                {link.label}
+              </Link>
+            );
+          })}
+        </div>
+      )}
     </nav>
   );
-}
+};
 
 export default Nav;
