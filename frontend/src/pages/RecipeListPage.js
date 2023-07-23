f1b7e4230844e46f3229f5ce55478f5308f64417
MarkMeltzer <33347173+MarkMeltzer@users.noreply.github.com>
2023-07-23 17:50:38 +0200

    Add filter to recipeListPage

--- frontend/src/pages/RecipeListPage.js
+++ frontend/src/pages/RecipeListPage.js
@@ -1,9 +1,11 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import useGetRecipes  from "../hooks/useGetRecipes";
 import { Link } from "react-router-dom";
 import "../css/shared/listPage.css"
 
 const RecipeListPage = () => {
+  const [filter, setFilter] = useState("")
+
   const {
     data: recipes, _, isLoading, error, sendRequest
   } = useGetRecipes();
@@ -15,23 +17,32 @@ const RecipeListPage = () => {
   return <div className="listContainer">
     {error && <p className='error'>Error: {error.message}</p>}
     {!error && isLoading && <p className='loading'>Loading....</p>}
-    {recipes &&
-      <Link to="/newrecipe" className='newItemButton'>
-        New Recipe
-      </Link>
-    }
-    {recipes &&
-      Object.entries(recipes).map((recipe) => {
+    {recipes && <>
+      <div className="filterAndNewItemButtonContainer">
+        <input
+          type="text"
+          className="listFilter"
+          placeholder="Type here to filter recipe name..."
+          onChange={e => setFilter(e.target.value)}
+          value={filter}
+        />
+        <Link to="/newrecipe" className='newItemButton'>
+          New Recipe
+        </Link>
+      </div>
+      {Object.entries(recipes).map((recipe) => {
         // recipe = [id, {...}]
-        return <div className="listItemContainer" key={recipe[0]}>
-          <Link
-            className='listItem'
-            to={`/recipe/${recipe[0]}`}
-          >
-            {recipe[1]?.name}
-          </Link>
-        </div>
-      })
+        if (recipe[1].name.toLowerCase().includes(filter.toLowerCase())) {
+          return <div className="listItemContainer" key={recipe[0]}>
+            <Link
+              className='listItem'
+              to={`/recipe/${recipe[0]}`}
+            >
+              {recipe[1]?.name}
+            </Link>
+          </div>}
+      })}
+    </>
     }
   </div>
 }
