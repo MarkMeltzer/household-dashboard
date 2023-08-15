6fc498f8e41330f85f0f1cf0c50b52c63e97ad88
MarkMeltzer <m.kirkeng@gmail.com>
2023-08-15 22:46:09 +0200

    Generate weekLists routes dynamically from base routes
    Rationale: for each new resource (for example for recipes) a bunch of basic resource endpoints had to be written for basic functionality such as listing all, creating new, updating and deleting records. This resulted in a lot of duplicated code (and all problems that come with that).
    
    Now these endpoints can be dynamically generated from base functions with relatively little code and while still allowing for creating fully custom endpoints for each route as before.
    
    The main idea is just to use partial functions and take advantage of the fact that decorators are just functions that take other functions as arguments.
    
    Flask does have some support for this use case but that seemed to still require more boilerplate and the whole idea is to keep boilerplate to a minimum.

--- backend/routes/weekLists.py
+++ backend/routes/weekLists.py
@@ -1,93 +1,17 @@
 from flask import Blueprint, request, abort, jsonify
 from utils import verify_token, get_datetime
 from database import Database
+from . import base
 
 blueprint = Blueprint('weekLists', __name__, url_prefix='/weekLists')
 
-@blueprint.route("", methods=["GET", "POST"])
-def all_week_lists():
-    '''
-    Endpoint for all weekLists.
+blueprint.route('', methods=['GET', 'POST'])(
+    base.generate_route(base.all_records, table='weekLists')
+)
 
-    - GET returns list of all weekList
-    - POST creates new weekList from data in request body
-    '''
-
-    # authorize client
-    if not verify_token(request):
-        print("Wrong token.")
-        abort(401)
-
-    db = Database()
-
-    if request.method == "GET":
-        print(f"{get_datetime()} -- Retrieving all weekList records...")
-
-        return jsonify(db.get_all_records('weekLists'))
-    elif request.method == "POST":
-        # add a new weeklist record
-        print(f"{get_datetime()} -- Adding new weekList record...")
-
-        record_id = db.add_record('weekLists', request.json)
-        
-        return jsonify({"id" : record_id})
-    else:
-        print(request.method + " not implemented for this route!")
-        abort(405)
-
-@blueprint.route("/<string:record_id>", methods=["GET", "PUT", "DELETE"])
-def specific_week_list(record_id):
-    '''
-    Endpoint for specific weekLists.
-
-    - GET returns the weekList with the provided id
-    - PUT replaces weekList with the provided id with the data provided in
-    the request body
-    - DELETE weekList from `db.json`, archive it to `archive.json`
-    '''
-
-    # authorize client
-    if not verify_token(request):
-        print("Wrong token.")
-        abort(401)
-
-    db = Database()
-
-    if not db.record_exists('weekLists', record_id):
-        abort(404)
-
-    if request.method == "GET":
-        # get existing weeklist record
-        print(
-            f"{get_datetime()} -- Retrieving weekList record with id: "
-            f"{record_id}..."
-        )
-
-        return jsonify(db.get_record('weekLists', record_id))
-    elif request.method == "PUT":
-        # change existing weeklist record
-        print(
-            f"{get_datetime()} -- Changing existing weeklist record with id: "
-            f"{record_id}..."
-        )
-
-        db.update_record('weekLists', record_id, request.json)
-
-        return jsonify({"id" : record_id})
-    elif request.method == "DELETE":
-        # delete existing weeklist record
-        print(
-            f"{get_datetime()} -- Deleting weeklist record with id: "
-            f"{record_id}..."
-        )
-
-        db.delete_record('weekLists', record_id)
-
-        return jsonify({"id" : record_id})
-    else:
-        print(request.method + " not implemented for this route!")
-        # TODO: make enum for HTTP status codes
-        abort(405)
+blueprint.route('/<string:record_id>', methods=['GET', 'PUT', 'DELETE'])(
+    base.generate_route(base.specific_record, table='weekLists')
+)
 
 @blueprint.route(
     "/<string:weekListId>/shoppingList/<int:index>",
