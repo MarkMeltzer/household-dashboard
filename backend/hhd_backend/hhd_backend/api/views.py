095a3d548050c23874e0660b87a32563dcf54746
MarkMeltzer <m.kirkeng@gmail.com>
2024-08-22 13:38:09 +0200

    Add linting

--- backend/hhd_backend/hhd_backend/api/views.py
+++ backend/hhd_backend/hhd_backend/api/views.py
@@ -3,11 +3,13 @@ from rest_framework import permissions, viewsets
 from hhd_backend.api.serializers import UserSerializer
 from hhd_backend.api.models import User
 
+
 # Create your views here.
 class UserViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
     """
-    queryset = User.objects.all().order_by('-date_joined')
+
+    queryset = User.objects.all().order_by("-date_joined")
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
--- backend/hhd_backend/hhd_backend/urls.py
+++ backend/hhd_backend/hhd_backend/urls.py
@@ -5,10 +5,10 @@ from rest_framework import routers
 from hhd_backend.api import views
 
 router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
+router.register(r"users", views.UserViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path("", include(router.urls)),
+    path("admin/", admin.site.urls),
+    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
 ]
