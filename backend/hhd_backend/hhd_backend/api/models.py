80e6ced578afc7aabf574b6f9fdbfe1eac97c020
MarkMeltzer <m.kirkeng@gmail.com>
2024-08-24 23:10:43 +0200

    Make ShoppingListItem a proper through model
    This allows for the ORM to provide some nicer queries. Forex. see here: https://old.reddit.com/r/django/comments/gsii8/new_to_django_whats_the_benefit_of_the_manytomany/
    
    It allows us to do queries like: `shopping_item_instance.shoppinglist_set.all()` to get all shopping lists containing an item and `shopping_list_instance.shopping_items.all()` to get all items in said shopping list.

--- backend/hhd_backend/hhd_backend/api/models.py
+++ backend/hhd_backend/hhd_backend/api/models.py
@@ -29,14 +29,6 @@ class MealsList(models.Model):
     week_list = models.OneToOneField(WeekList, on_delete=models.CASCADE)
 
 
-class ShoppingList(models.Model):
-    week_list = models.OneToOneField(WeekList, on_delete=models.CASCADE)
-
-    def __str__(self) -> str:
-        week_list = getattr(self, 'week_list', None)
-        return f'For weeklist "{week_list}"'
-
-
 class ShoppingItem(models.Model):
     household = models.ForeignKey(Household, on_delete=models.CASCADE)
 
@@ -46,8 +38,16 @@ class ShoppingItem(models.Model):
         return self.name
 
 
-# TODO: this might be improved by adding a manyToMany relationship to shoppingItem and
-# point the 'through' arg to this model. But not entirely sure what the improvement is :P
+class ShoppingList(models.Model):
+    week_list = models.OneToOneField(WeekList, on_delete=models.CASCADE)
+
+    shopping_items = models.ManyToManyField(ShoppingItem, through='ShoppingListItem')
+
+    def __str__(self) -> str:
+        week_list = getattr(self, 'week_list', None)
+        return f'For weeklist "{week_list}"'
+
+
 class ShoppingListItem(models.Model):
     shopping_list = models.ForeignKey(ShoppingList, on_delete=models.CASCADE)
     shopping_item = models.ForeignKey(ShoppingItem, on_delete=models.CASCADE)
